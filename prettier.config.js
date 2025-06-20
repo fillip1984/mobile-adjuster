e9b1e1b035e7f0b4ab341f36bc0548a79d11d42b
fillip1984 <fillip1984@gmail.com>
2025-06-20 15:50:16 -0400

    removing stuff I won't need

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  bracketSameLine: true,
+  // trailingComma: "es5",
+  // tabWidth: 4,
+  // semi: false,
+  // singleQuote: true,
+};
+
+module.exports = config;
